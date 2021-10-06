356571cbba40f304c38cc3d706987abaa3a501cd
Ludvik <luddmachine@gmail.com>
2021-10-07 00:33:44 +0200

    pallette fix

--- src/App.js
+++ src/App.js
@@ -5,31 +5,13 @@ import Music from "./pages/Music/Music";
 import About from "./pages/About/About";
 import Gallery from "./pages/Gallery/Gallery";
 import Contact from "./pages/Contact/Contact";
-import Palette1 from "./assets/png/palette-1.png";
-import Palette2 from "./assets/png/palette-2.png";
 
 const App = () => {
 	return (
 		<Router>
 			<Header />
-			<div className="relative">
-				<div className="">
-					<img
-						src={Palette1}
-						className="sticky top-0 bottom-0"
-						alt=""
-						srcset=""
-					/>
-					<img
-						src={Palette2}
-						className="sticky top-0 bottom-0"
-						alt=""
-						srcset=""
-					/>
-				</div>
-			</div>
 			<Switch>
-				<main>
+				<main className="m-0 p-0">
 					<Route exact path="/" component={Music} />
 					<Route exact path="/about" component={About} />
 					<Route exact path="/gallery" component={Gallery} />
--- src/pages/Music/Music.js
+++ src/pages/Music/Music.js
@@ -1,9 +1,46 @@
 import React from "react";
+import Palette1 from "../../assets/png/palette-1.png";
+import Palette2 from "../../assets/png/palette-2.png";
 
 const Music = () => {
 	return (
-		<div className="h-80">
-			<h1 className="text-blue-500 text-9xl">Music</h1>
+		<div>
+			<div className="flex justify-between relative">
+				<div className="">
+					<img
+						src={Palette1}
+						className="sticky top-0 h-screen"
+						alt=""
+						srcset=""
+					/>
+				</div>
+				<div className="w-screen py-12">
+					<div className="max-w-3xl m-auto">
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+						<h1 className="text-blue-500 text-9xl">Music</h1>
+					</div>
+				</div>
+				<div className="">
+					<img
+						src={Palette2}
+						className="sticky top-0 h-screen"
+						alt=""
+						srcset=""
+					/>
+				</div>
+			</div>
 		</div>
 	);
 };
